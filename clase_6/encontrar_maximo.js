b756a6fcf2d6d4a12307a47fcd159dba47e94762
Daiana <daianapey@hotmail.com>
2024-05-19 10:32:34 -0300

    entregable3

--- clase_6/encontrar_maximo.js
+++ clase_6/encontrar_maximo.js
@@ -1,15 +1,21 @@
+/*Dado el siguiente arreglo
+[4,7,9,3,1,45,67,23,29,78,11,16]
+- Crear un programa que encuentre cuál es el número
+más grande del arreglo e imprimirlo por consola
+- Almacenar el número más grande en una variable
+global y pasarlo a una función para determinar si el
+número es par o impar*/
 var arregloNumMax = [4, 7, 9, 3, 1, 45, 67, 23, 29, 78, 11, 16];
 var numeroMax = 0;
-var contador;
-for (contador = 0; contador < 12; contador++) {
-    if (arregloNumMax[contador] > numeroMax)
-        numeroMax = arregloNumMax[contador];
+var indice;
+for (indice = 0; indice < 12; indice++) {
+    if (arregloNumMax[indice] > numeroMax)
+        numeroMax = arregloNumMax[indice];
 }
 console.log("El número más grande es:" + numeroMax);
-
+paridad(numeroMax);
 // Función para determinar si el número es par o impar
 function paridad(numeroMax) {
-    console.log("El número más grande es:" + numeroMax);
     if (numeroMax % 2 === 0) {
         console.log(numeroMax + " es un número par.");
     }
@@ -18,4 +24,3 @@ function paridad(numeroMax) {
     }
 }
 // Determinar si el número más grande es par o impar
-paridad(numeroMax);
--- clase_6/encontrar_maximo.ts
+++ clase_6/encontrar_maximo.ts
@@ -1,18 +1,28 @@
+/*Dado el siguiente arreglo
+[4,7,9,3,1,45,67,23,29,78,11,16]
+- Crear un programa que encuentre cuál es el número
+más grande del arreglo e imprimirlo por consola
+- Almacenar el número más grande en una variable
+global y pasarlo a una función para determinar si el
+número es par o impar*/
+
 let arregloNumMax : number[] = [4,7,9,3,1,45,67,23,29,78,11,16];
-let numeroMax:number =0;
-let contador:number;
+let numeroMax:number=0;
+let indice:number;
 
 
-for (contador = 0; contador<12; contador++){
-    if (arregloNumMax[contador] > numeroMax)
-        numeroMax= arregloNumMax[contador]
+for (indice = 0; indice<12; indice++){
+    if (arregloNumMax[indice] > numeroMax)
+        numeroMax= arregloNumMax[indice]
 }
 console.log("El número más grande es:" +numeroMax); 
 
+paridad(numeroMax);
+
 
 // Función para determinar si el número es par o impar
+
 function paridad(numeroMax: number){
-    console.log("El número más grande es:" +numeroMax); 
     if (numeroMax % 2 === 0) {
         console.log(numeroMax+" es un número par.");
     } else {
@@ -20,4 +30,3 @@ function paridad(numeroMax: number){
     }
 }
 // Determinar si el número más grande es par o impar
-paridad(numeroMax);
